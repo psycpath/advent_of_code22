db7893b2dc47abfcb8aeaf6634cdfe7715ef0899
Ali <ali.marmi@hotmail.com>
2022-12-23 20:15:30 +0100

    Solved day 1 part 1

--- days/1/day1.py
+++ days/1/day1.py
@@ -1,14 +1,22 @@
 print('\nOutput for challenge day 1\n')
 
-with open('days/1/input_simple.txt') as file:
+with open('days/1/input.txt') as file:
     lines = file.readlines()
 
+lines.append('\n')
+
 total = 0
+total_sums = list()
+
+# TO ADD TO A LIST: total_sums.append(VALUE)
 for line in lines:
     line = line.strip()
     if line == "":
+        total_sums.append(total)
+        total = 0
         continue
 
     line = int(line)
     total = total + line
-print(total)
+total_sums.sort()
+print("Largest element is:",total_sums[-1])
