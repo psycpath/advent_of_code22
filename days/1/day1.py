d50dc080e4336e2bbd477ab3e0626ed0fa358218
psycpath <ali.marmi@hotmail.com>
2022-12-24 17:46:14 +0100

    Finished Day 2 Part 1

--- days/1/day1.py
+++ days/1/day1.py
@@ -3,8 +3,26 @@ print('\nOutput for challenge day 1\n')
 with open('days/1/input.txt') as file:
     lines = file.readlines()
 
-print(lines[0])
+lines.append('\n')
 
+total = 0
+total_sums = list()
+
+# TO ADD TO A LIST: total_sums.append(VALUE)
 for line in lines:
-    if line.strip() != "":
-        print(line)
+    line = line.strip()
+    if line == "":
+        total_sums.append(total)
+        total = 0
+        continue
+
+    line = int(line)
+    total = total + line
+
+total_sums.sort()
+total_sums.reverse()
+
+top_3 = total_sums[0] + total_sums[1] + total_sums[2]
+
+print("Largest element is:", total_sums[0])
+print("The top three elves are carrying a total of", top_3)
--- days/2/day2.py
+++ days/2/day2.py
@@ -1,1 +1,34 @@
 print('\nOutput for challenge day 2\n')
+
+with open('days/2/input.txt') as file:
+    lines = file.readlines()
+
+total = list()
+
+
+for line in lines:
+    line = line.strip()
+    if  line == "A X":
+        total.append(4)
+    elif line == "B X":
+        total.append(1)
+    elif line == "C X":
+        total.append(7)
+    elif line == "A Y":
+        total.append(8)
+    elif line == "B Y":
+        total.append(5)
+    elif line == "C Y":
+        total.append(2)
+    elif line == "A Z":
+        total.append(3)
+    elif line == "B Z":
+        total.append(9)
+    elif line == "C Z":
+        total.append(6)
+
+print(sum(total))
+        
+
+    
+
