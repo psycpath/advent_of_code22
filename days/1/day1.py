b8d20c2886ebd00c86db19133184ce8752313209
Ali <ali.marmi@hotmail.com>
2022-12-24 11:23:17 +0100

    DAY + Part 1 + 2 DONE

--- days/1/day1.py
+++ days/1/day1.py
@@ -18,5 +18,11 @@ for line in lines:
 
     line = int(line)
     total = total + line
+
 total_sums.sort()
-print("Largest element is:",total_sums[-1])
+total_sums.reverse()
+
+top_3 = total_sums[0] + total_sums[1] + total_sums[2]
+
+print("Largest element is:", total_sums[0])
+print("The top three elves are carrying a total of", top_3)
