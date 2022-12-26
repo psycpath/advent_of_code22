e36da2deb42965a333663ec4f420e4d9244220db
Ewald Kleefstra <kleefstra@gyselroth.com>
2022-12-26 11:50:03 +0100

    remove test files and move everything to main file also remove comments

--- days/3/day3.py
+++ days/3/day3.py
@@ -3,34 +3,28 @@ print('\nOutput for challenge day 2\n')
 with open('days/3/input.txt') as file:
     lines = file.readlines()
 
-Allintersections = list()
-    
-    
+intersections = list()
+prioritylist = list()
+
 for line in lines:
     line = line.strip()
-    first_compartment =  line[slice(0,len(line)//2)]
-    second_compartment =  line[slice(len(line)//2, len(line))]
+    first_compartment = line[:len(line)//2]
+    second_compartment = line[len(line)//2 : len(line)]
 
-    set1 = (set(first_compartment))
-    set2 = (set(second_compartment))
-    intersec = (set1.intersection(set2))
-    Allintersections.append(intersec)
+    set1 = set(first_compartment)
+    set2 = set(second_compartment)
 
+    all_intersections = set1.intersection(set2)
 
+    intersection = list(all_intersections)[0]
+    intersections.append(intersection)
 
-for line in Allintersections:
-    prioritylist = list()
-    if any(letter.isupper() for letter in line):
-        number = [ord(char) - 40 for char in line]
+for intersection in intersections:
+    if intersection.isupper():
+        priority = ord(intersection) - 38
     else:
-        number = [ord(char) - 96 for char in line]
-    prioritylist.append(number)
+        priority = ord(intersection) - 96
 
-print(prioritylist)
+    prioritylist.append(priority)
 
-
-
-    
-
-
-
+print(sum(prioritylist))
