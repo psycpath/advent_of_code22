2df50b5c97a98840fcacf386e252f96fcd4962e7
psycpath <ali.marmi@hotmail.com>
2022-12-30 16:47:18 +0100

    Day4 cleaned up

--- days/4/day4.py
+++ days/4/day4.py
@@ -7,11 +7,12 @@ total = list()
 totalintersections = list()
 
 for line in lines:
-    line = line.strip()
-    set1 = line.split(",")
 
-    first_part = set1[0]
-    second_part = set1[1]
+    line = line.strip()
+    pair = line.split(",")
+
+    first_part = pair[0]
+    second_part = pair[1]
     first_part = first_part.split("-")
     second_part = second_part.split("-")
 
@@ -20,24 +21,20 @@ for line in lines:
     second_part_firstnum = int(second_part [0])
     second_part_secondnum = int(second_part [1])
 
-    list1 = list()
-    list2 = list()
+    set1 = set()
+    set2 = set()
 
     for numbers in range(first_part_firstnum, first_part_secondnum + 1):
-        list1.append(numbers)
-
+        set1.add(numbers)
     for numbers in range(second_part_firstnum, second_part_secondnum + 1):
-        list2.append(numbers)
-
-    set1 = set(list1)
-    set2 = set(list2)
+        set2.add(numbers)
 
     if set1.issubset(set2):
         total.append(1)
-
     elif set2.issubset(set1):
         total.append(1)
 
+    #Part 2--
     if set1.intersection(set2) != set():
         totalintersections.append(1)
 
