c4368c628cfe7d76c683dfa3d67ee7aa1a86c505
psycpath <ali.marmi@hotmail.com>
2023-01-04 19:17:14 +0100

    Day 5 line.replace

--- days/5/day5.py
+++ days/5/day5.py
@@ -17,25 +17,7 @@ stacks = [
 
 for line in lines:
     line = line.strip()
-    numeric_filter = filter(str.isdigit, line)
-    numeric_string = "".join(numeric_filter)
     line.replace("move ", "")
-
-    if len(numeric_string) == 3:
-        amount_of_crates = numeric_string[0]
-        place_of_extraction = numeric_string[1]
-        place_of_insertion = numeric_string[2]
-    else:
-        amount_of_crates = numeric_string[:2]
-        place_of_extraction = numeric_string[2]
-        place_of_insertion = numeric_string[3]
-
-    amount_of_crates = int(amount_of_crates)
-    place_of_extraction = "".join(("stack", place_of_extraction))
-    place_of_insertion = "".join(("stack", place_of_insertion))
-
-    place_of_extractionv = vars(place_of_extraction)
-    place_of_insertion = vars(place_of_insertion)
-
-    place_of_extraction.append(S)
-
+    line.replace("from", " ")
+    line.replace("to", " ")
+    print(line)
