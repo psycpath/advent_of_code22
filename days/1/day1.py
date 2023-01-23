508a33ae5b5a13da73bd587bf00169c7cf2ace84
Ali <ali.marmi@hotmail.com>
2023-01-23 21:42:20 +0100

    suggestions ewald

--- days/1/day1.py
+++ days/1/day1.py
@@ -18,7 +18,6 @@ for line in lines:
 
     line = int(line)
     total = total + line
-    print(line)
 
 total_sums.sort()
 total_sums.reverse()
@@ -27,3 +26,4 @@ top_3 = total_sums[0] + total_sums[1] + total_sums[2]
 
 print("Largest element is:", total_sums[0])
 print("The top three elves are carrying a total of", top_3)
+print()
--- days/7/day7.py
+++ days/7/day7.py
@@ -1,8 +1,40 @@
+class Directory:
+    name = ""
+    files = list()
+    directories = dict()
+
+    def __init__(self, name):
+        self.name = name
+
+
+def change_directory(current_dir, directory_name):
+    if directory_name == '..':
+        print('Returning parent directory from current directory')
+        # return parent directory
+
+    current_dir = current_dir.directories[directory_name]
+
+    return current_dir
+
+
 print('\nOutput for challenge day 7\n')
 
-with open('days/7/input.txt') as file:
+current_dir = Directory('/')
+
+with open('days/7/input_simple.txt') as file:
     lines = file.readlines()
 
-class Directory:
-    files = list()
-    direcotires = list(Directory)
+for line in lines[1:]:
+    if line.startswith("$ cd"):
+        line_parts = line.split(' ')
+        directory_name = line_parts[2]
+        current_dir = change_directory(current_dir, directory_name)
+    else:
+        # is "ls" command or a line that follows it
+        if line.startswith("dir"):
+            line_parts = line.split(' ')
+            dir_name = line_parts[1]
+            new_directory = Directory(dir_name)
+            current_dir.directories[dir_name] = new_directory
+        else:
+            print("is a file")
