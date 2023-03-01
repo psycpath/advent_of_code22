510766fbe2b608cc4e0b5c517c1aa7a08dc53e71
psycpath <ali.marmi@hotmail.com>
2023-03-01 15:42:13 +0100

    Nim2 richtig

--- days/Informatik Schule/streichhölzer.py
+++ days/Informatik Schule/streichhölzer.py
@@ -1,22 +1,34 @@
 from random import randint
 
-x = randint(12,20)
-
 print("Nimm Spiel")
 print("")
 print("Wer das letzte Streichholz nehmen muss, hat verloren!")
-print("Anzahl Streichhölzer:", x)
-
-spielzug = int(input("Wie viele Streichhölzer nehmen Sie? [1,2,3] "))
-if x = 1:
-    print("Spieler hat Gewonnen")
-print("Anzahl Streichhölzchen: ", x - spielzug)
-
-x -= spielzug
 
 
-computerspielzug = (x-1)%4
+x = randint(12,20)
 
-print("Computer nimmt", computerspielzug)
-x -= computerspielzug
-print("Anzahl Streichhölzer:", x)
+while x != 1:
+
+    print("Anzahl Streichhölzer:", x)
+    print("")
+    spielzug = int(input("Wie viele Streichhölzer nehmen Sie? [1,2,3] "))
+    if spielzug =! 1 or spielzug =!2 or spielzug =!3
+
+
+    x -= spielzug
+    if x == 1:
+        print("Spieler hat gewonnen")
+        break
+
+    print("Anzahl Streichhölzer:", x)
+    print("")
+
+    computerspielzug = (x-1)%4
+    if (x-1)%4 == 0:
+        computerspielzug = randint(1,3)
+
+    print("Computer nimmt", (x-1)%4, "Hölzchen")
+    x -= computerspielzug
+    if x == 1:
+        print("Computer hat gewonnen")
+
